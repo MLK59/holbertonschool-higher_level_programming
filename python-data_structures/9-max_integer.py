ad1fa70a1a4feb264337e4acf151ad55bef5d7ca
MLK59 <abdelmalekmhamed@gmail.com>
2024-01-30 13:14:06 +0100

    find the maximum value in a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    current_max = my_list[0]
+    for num in my_list[1:]:
+        if num > current_max:
+            current_max = num
+
+    return current_max
