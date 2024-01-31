1201a16194d58cf257997163859f219b23c56293
MLK59 <abdelmalekmhamed@gmail.com>
2024-01-31 23:00:35 +0100

    Refactor square_matrix_simple function to use list comprehension

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,8 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    new_matrix = matrix.copy()
 
-    for row in matrix:
-        new_row = []
-        for item in row:
-            new_item = item ** 2
-            new_row.append(new_item)
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return (new_matrix)
