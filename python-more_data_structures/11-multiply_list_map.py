de5af900f2eea0dec8333825e6809e27c243980c
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-02 11:26:16 +0100

    Refactor multiply_list_map function to use list comprehension

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+# This function returns a list with all values multiplied by a number without using any loops
 def mutiply_list_map(my_list=[], number=0):
-    return(map(lambda x: x * number,my_list))
+    return list(map((lambda x: x * number), my_list))
