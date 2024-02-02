d5db170294bec28580a2ccbaa106d28b50f837d2
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-02 23:39:53 +0100

    Mise à jour des fonctions Python dans les structures de données

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	length = len(sentence)
-	if length == 0:
-		first = None
-	else:
-		first = sentence[0]
-	return (length, first)
+    length = len(sentence)
+    if length == 0:
+        first = None
+    else:
+        first = sentence[0]
+    return (length, first)
