ba7cb4b7569f36bd2e7aa4c0cf8b1333fad6ce26
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-07 23:01:17 +0100

    addition of a comment and modification of the size attribute.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0):
-        self._Square__size = size
+    """
+    creates a square class object
+    """
+    def __init__(self, size):
+        self.__size = size
