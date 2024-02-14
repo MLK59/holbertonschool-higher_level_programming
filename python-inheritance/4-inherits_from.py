fe6b5f6c5eea8d57dab121f4a0a96eefab94a7cb
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-14 15:20:18 +0100

    Implement a function that evaluates whether a given object is an instance of a class t

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" subclass checker
+    Parameters:
+    obj (object): The object to check.
+    cls (class): The specified class to check against.
+
+    Returns:
+    bool: True if obj is an instance of a class that inherited from cls, False otherwise.
+    """
+
+
+def inherits_from(obj, a_class):
+    """returns subclass yes"""
+    if type(obj) == a_class:
+        return False
+    return issubclass(type(obj), a_class)
