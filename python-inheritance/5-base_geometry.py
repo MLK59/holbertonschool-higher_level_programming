362c73e1f6b1b3ac1584db11d5a56154d4aace20
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-14 15:27:17 +0100

    define an empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """
+    BaseGeometry class definition.
+    
+    This is an empty class that serves as a base class for geometry-related operations.
+    """
+    
+    pass 
