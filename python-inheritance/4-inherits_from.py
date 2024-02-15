331c74819ad864accfa7fdc583e64821ed69bccd
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-15 12:52:55 +0100

    update pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,9 +14,10 @@ False otherwise.
 
 def inherits_from(obj, a_class):
     """
-    returns True if obj is an instance of a class that inherited 
-    from a_class,or if obj is an instance of a_class itself. False otherwise.
+    returns True if obj is an instance of a class that inherited
+    from a_class,or if obj is an instance of a_class itself.
+    False otherwise.
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if isinstance(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
