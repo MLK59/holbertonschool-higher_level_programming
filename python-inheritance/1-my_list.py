1d4f92644cb1d153e7ca75c37cf44c78a5a75a4d
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-15 12:40:51 +0100

    Changes to the inherits_from function to check whether an object is an instance of an inherited class or of the class itself.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ Class that inherits from list
 
 
 class MyList(list):
-
+    """ class that inherits from list """
     def print_sorted(self):
         """
         prints list in ascending sort
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,12 +7,16 @@ obj (object): The object to check.
 cls (class): The specified class to check against.
 
 Returns:
-bool: True if obj is an instance of a class that inherited from cls, False otherwise.
+bool: True if obj is an instance of a class that inherited from cls,
+False otherwise.
 """
 
 
 def inherits_from(obj, a_class):
-    """returns subclass yes"""
-    if type(obj) == a_class:
-        return False
-    return issubclass(type(obj), a_class)
+    """
+    returns True if obj is an instance of a class that inherited 
+    from a_class,or if obj is an instance of a_class itself. False otherwise.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
