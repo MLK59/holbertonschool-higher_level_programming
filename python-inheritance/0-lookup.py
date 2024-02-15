19fd313dc87beedb29c612e1f2ca59af3f2da803
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-15 12:25:47 +0100

    Changes to the lookup function to improve code readability

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Returns the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    """
+    """returns list of available attributes and methods of an object"""
     return dir(obj)
