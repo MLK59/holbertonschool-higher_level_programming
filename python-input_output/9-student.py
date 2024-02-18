1dd84d7bc08ca22e1cd00a11d6e640db9b517a0d
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-18 22:29:00 +0100

    improve the code and pycodestyle

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" Class to JSON Function """
+
+"""class_to_json
+"""
 
 
 def class_to_json(obj):
-    """Return the dictionary representation of simple data structure"""
+    """Returns the dictionary description for JSON"""
+
     return obj.__dict__
