d18fc7a01eb9b9ddd704fb9200b60dc20be59411
MLK59 <abdelmalekmhamed@gmail.com>
2024-02-18 22:22:32 +0100

    upload and improve the code

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-""" define class Student """
+"""student class"""
 
 
 class Student:
-    """class Student"""
+    """class stuff"""
 
     def __init__(self, first_name, last_name, age):
-        """initialize class Student
-        Args:
-            first_name (str): first name
-            last_name (str): last name
-            age (int): age
-        """
+        """initializes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of Student
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if attrs is None:
+        """prints __dict__"""
+        if attrs and isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {x: y for x, y in self.__dict__.items() if x in attrs}
+        else:
             return self.__dict__
-        if all(isinstance(attr, str) for attr in attrs):
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
 
     def reload_from_json(self, json):
-        """Replace all attributes of the student instance
-        Args:
-            json(dict): key/value to replace attributes with
-        """
-        for key, value in json.items():
-            setattr(self, key, value)
+        """replaces allattributes with json"""
+        if json:
+            self.__dict__ = json
