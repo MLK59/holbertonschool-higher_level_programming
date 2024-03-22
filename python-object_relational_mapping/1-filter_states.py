d3f95cf57d0322001056ac1044d5bc0be7584dd2
MLK59 <abdelmalekmhamed@gmail.com>
2024-03-22 23:52:23 +0100

    improve the code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,22 @@
 from the database `hbtn_0e_0_usa`.
 """
 
+from sys import argv
+import MySQLdb
+
+
 if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
 
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         password=argv[2], db=argv[3], port=3306)
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
 
-    cur.execute("SELECT id, name FROM states WHERE name \
-        LIKE BINARY 'N%' ORDER BY states.id;")
-
-    for row in cur.fetchall():
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
-    db.close()
+    cur.close()
+    conn.close()
+    
